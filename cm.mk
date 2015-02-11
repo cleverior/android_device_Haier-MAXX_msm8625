8934010853e6908fa306efc5227763c402cb290e
cleverior <dcleverior@gmail.com>
2015-02-11 22:12:54 +0700

    first commit

---
+++ cm.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := msm8625
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Haier_MAXX/msm8625/device_msm8625.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := msm8625
+PRODUCT_NAME := cm_msm8625
+PRODUCT_BRAND := qcom
+PRODUCT_MODEL := msm8625
+PRODUCT_MANUFACTURER := Haier_MAXX
+PRODUCT_BOARD := 7x27
+DEVICE_MODEL := msm8625
---
+++ device_msm8625.mk
@@ -0,0 +1,60 @@
+LOCAL_PATH := device/Haier_MAXX/msm8625
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/Haier_MAXX/msm8625/msm8625-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/Haier_MAXX/msm8625/overlay
+
+LOCAL_PATH := device/Haier_MAXX/msm8625
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+# Additional default prop
+ADDITIONAL_DEFAULT_PROPERTIES += \
+	ro.secure=1 \
+	ro.allow.mock.location=0 \
+	ro.debuggable=1 \
+	persist.sys.usb.config=mtp,mass_storage,adb \
+	ro.goso.default.usb.config=mtp,mass_storage
+
+# Additional mmc fstab
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/etc/recovery_mmc.fstab:recovery/root/etc/recovery_mmc.fstab 
+
+# Charging images
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/res/images/charger/battery_0.png:recovery/root/res/images/charger/battery_0.png \
+	$(LOCAL_PATH)/res/images/charger/battery_1.png:recovery/root/res/images/charger/battery_1.png \
+	$(LOCAL_PATH)/res/images/charger/battery_2.png:recovery/root/res/images/charger/battery_2.png \
+	$(LOCAL_PATH)/res/images/charger/battery_3.png:recovery/root/res/images/charger/battery_3.png \
+	$(LOCAL_PATH)/res/images/charger/battery_4.png:recovery/root/res/images/charger/battery_4.png \
+	$(LOCAL_PATH)/res/images/charger/battery_5.png:recovery/root/res/images/charger/battery_5.png \
+	$(LOCAL_PATH)/res/images/charger/battery_charge.png:recovery/root/res/images/charger/battery_charge.png \
+	$(LOCAL_PATH)/res/images/charger/battery_fail.png:recovery/root/res/images/charger/battery_fail.png \
+	$(LOCAL_PATH)/res/images/charger/num0.png:recovery/root/res/images/charger/num0.png \
+	$(LOCAL_PATH)/res/images/charger/num1.png:recovery/root/res/images/charger/num1.png \
+	$(LOCAL_PATH)/res/images/charger/num2.png:recovery/root/res/images/charger/num2.png \
+	$(LOCAL_PATH)/res/images/charger/num3.png:recovery/root/res/images/charger/num3.png \
+	$(LOCAL_PATH)/res/images/charger/num4.png:recovery/root/res/images/charger/num4.png \
+	$(LOCAL_PATH)/res/images/charger/num5.png:recovery/root/res/images/charger/num5.png \
+	$(LOCAL_PATH)/res/images/charger/num6.png:recovery/root/res/images/charger/num6.png \
+	$(LOCAL_PATH)/res/images/charger/num7.png:recovery/root/res/images/charger/num7.png \
+	$(LOCAL_PATH)/res/images/charger/num8.png:recovery/root/res/images/charger/num8.png \
+	$(LOCAL_PATH)/res/images/charger/num9.png:recovery/root/res/images/charger/num9.png \
+	$(LOCAL_PATH)/res/images/charger/percent.png:recovery/root/res/images/charger/percent.png
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_msm8625
+PRODUCT_DEVICE := msm8625
